b7845d077ca054d608eb5016c5a8fe81bb154bf1
Aashish Kumar <ashish8803568@gmail.com>
2025-04-25 09:41:21 +0530

    Add files via upload

---
+++ JS DOM 3/index.js
@@ -0,0 +1,60 @@
+// //adding 100para
+
+// const t1 = performance.now();
+// for(let i=1; i<=100; i++) {
+//     let newElement = document.createElement('p')
+//     newElement.textContent = 'Thid is Para' + i;
+
+//     document.body.appendChild(newElement);
+// }
+// const t2 = performance.now();
+// console.log("this took" +(t2-t1) + "ms");
+
+// // optimising a bit
+// const t3 = performance.now();
+// let myDiv = document.createElement('div');
+
+// for(let i=1; i<=100; i++) {
+//    let element = document.createElement('p');
+//    element.textContent = 'This is Para' +i;
+   
+//    myDiv.appendChild(element);
+// }
+
+// document.body.appendChild(myDiv);
+// const t4 = performance.now();
+// console.log("this took" +(t4-t3) + "ms");
+
+
+
+// let fragment = document.createDocumentFragment();
+
+// for(let i=1; i<=100; i++) {
+//     let newElement = document.createElement('p')
+//     newElement.textContent = 'Thid is Para' + i;
+
+//     fragment.appendChild(newElement);
+
+// }
+
+// document.body.appendChild(fragment);// single reflow , single Repaint
+
+
+
+// SINGLE THREADING
+
+function addPara() {
+    let para= document.createElement('p');
+    para.textContent = 'Js is Single';
+    document.body.appendChild(para);
+
+}
+
+function appendNewMessage() {
+    let para = document.createElement('p');
+    para.textContent = 'How Are You';
+    document.body.appendChild(para);
+}
+
+addPara();
+appendNewMessage();
